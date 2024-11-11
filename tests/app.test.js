14e13f6400a6807d9285180d1b3e8a1d4c9c6e48
Sai Kiran <saikiran.diddi@thebluespire.com>
2024-11-11 12:33:44 +0530

    first-commit

---
+++ tests/app.test.js
@@ -0,0 +1,9 @@
+describe("Sample tests", ()=>{
+    test("First testcase", () => {
+        expect(1+1).toBe(2);
+    })
+
+    test("Second testcase", ()=>{
+        expect(1+2).toBe(3);
+    })
+})
